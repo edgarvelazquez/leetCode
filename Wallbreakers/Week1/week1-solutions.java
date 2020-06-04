b21dd340a8eadd0fd8485ce3cd5ac85755efed4f
edgarvelazquez <edgar.ra.velazquez@gmail.com>
2020-06-04 14:19:12 -0700

    Update week1-solutions.java

--- Wallbreakers/Week1/week1-solutions.java
+++ Wallbreakers/Week1/week1-solutions.java
@@ -36,9 +36,22 @@ class Solution {
 //If the array is Not sorted this is an optimal solution
 //I will create a hash map that stores values as it goes through the array
 //It will then try to find the values in the hash map
+import java.util.HashMap; 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+    	HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();//HashMap to store values
+    	int look=0;
+    	for(int i=0; i< nums.length;i++){
+    		look=target-nums[i]; //target value
+    		if(map.containsKey(look)){ //check if the key is already there
+    			return new int [] {map.get(look),i}; //return new array with position values
+    		}else{
+                map.put(nums[i],i); //if the hasmap does not contain the key add the values
+            }
+    	}
+    	 throw new IllegalArgumentException("No solution for input"); //I could also return null
 
-     
      }
 }
+
+
